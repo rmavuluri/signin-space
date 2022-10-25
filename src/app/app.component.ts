851209ab02ac94a922ce23b1d39551bc35395ce4
rmavuluri <rummy.mavuluri@outlook.com>
2022-10-24 21:24:40 -0400

    login page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sigin-space';
+  username: string = '';
+  password: string = '';
+
+  @Output()
+  change: EventEmitter<object> = new EventEmitter<object>();
+
+  login() {
+    console.log(this.username, this.password);
+    const loginData = {
+      username: this.username,
+      password: this.password,
+    };
+    console.log(loginData);
+    this.change.emit(loginData);
+  }
 }
